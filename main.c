814b96df75ece6dafcb4db358b220d9bbf7de8b8
Nils Streedain <nils@streedain.com>
2023-01-22 17:33:47 -0800

    Added Hello World

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  program1
+//
+//  Created by Nils Streedain on 1/22/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}

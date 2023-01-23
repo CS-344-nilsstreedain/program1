5d6f1436ad24b57eb0530fd11eaaaace9770a240
Nils Streedain <nils@streedain.com>
2023-01-22 20:32:38 -0800

    Rework and adding list functions

--- main.c
+++ main.c
@@ -39,21 +39,19 @@ struct movie *createMovie(char *currLine) {
 	strcpy(currMovie->rating, token);
 	
 	currMovie->next = NULL;
-	
 	return currMovie;
 }
 
-struct movie *processFile(char *filePath) {
-	FILE *movieFile = fopen(filePath, "r");
-	
-	char *currLine = NULL;
-	size_t len = 0;
-	ssize_t nread;
-	
+struct movie *parseFile(const char *filePath) {
 	struct movie *head = NULL;
 	struct movie *tail = NULL;
 	
-	while ((nread = getline(&currLine, &len, movieFile)) != -1) {
+	FILE *movieFile = fopen(filePath, "r");
+	char *currLine = NULL;
+	size_t len = 0;
+	
+	getline(&currLine, &len, movieFile);
+	while (getline(&currLine, &len, movieFile) != -1) {
 		struct movie *newNode = createMovie(currLine);
 		
 		if (head == NULL) {
@@ -69,15 +67,52 @@ struct movie *processFile(char *filePath) {
 	return head;
 }
 
-void printMovie(struct movie* aMovie) {
-	printf("%s, %s, %s, %s\n", aMovie->title, aMovie->year, aMovie->lang, aMovie->rating);
+int getLength(struct movie *list) {
+	int length = 0;
+	while (list != NULL) {
+		length++;
+		list = list->next;
+	}
+	return length;
 }
 
 void printMovieList(struct movie *list) {
 	while (list != NULL) {
-		printMovie(list);
+		printf("%s, %s, %s, %s\n", list->title, list->year, list->lang, list->rating);
 		list = list->next;
 	}
+}
+
+void listMovieListByYear(struct movie *list) {
+	printf("Enter the year for which you want to see movies: ");
+	int year, found = 0;
+	scanf("%d", &year);
+	while (list != NULL) {
+		if (atoi(list->year) == year) {
+			printf("%s\n", list->title);
+			found = 1;
+		}
+		list = list->next;
+	}
+	if (!found)
+		printf("No data about movies released in the year %d\n", year);
+	printf("\n");
+}
+
+void listMovieListByLang(struct movie *list) {
+	printf("Enter the language for which you want to see movies: ");
+	int year, found = 0;
+	scanf("%d", &year);
+	while (list != NULL) {
+		if (atoi(list->year) == year) {
+			printf("%s\n", list->title);
+			found = 1;
+		}
+		list = list->next;
+	}
+	if (!found)
+		printf("No data about movies released in the year %d\n", year);
+	printf("\n");
 }
 
 int main(int argc, const char * argv[]) {
@@ -86,8 +121,30 @@ int main(int argc, const char * argv[]) {
 		printf("Example usage: ./students student_info1.txt\n");
 		return EXIT_FAILURE;
 	}
-	struct movie *list = processFile(argv[1]);
-	printMovieList(list);
 	
-	return EXIT_SUCCESS;
+	struct movie *list = parseFile(argv[1]);
+	printf("Processed file %s and parsed data for %d movies\n", argv[1], getLength(list));
+	
+	while (1) {
+		printf("1. Show movies released in the specified year\n2. Show highest rated movie for each year\n3. Show the title and year of release of all movies in a specific language\n4. Exit from the program\n\nEnter a choice from 1 to 4: ");
+		
+		int userInput;
+		scanf("%d", &userInput);
+		switch(userInput) {
+			case 1:
+				listMovieListByYear(list);
+				break;
+			case 2:
+				printf("2\n");
+				break;
+			case 3:
+//				printf("Enter the language for which you want to see movies: ");
+				
+				break;
+			case 4:
+				return EXIT_SUCCESS;
+			default:
+				printf("You entered an incorrect choice. Try again.\n\n");
+		}
+	}
 }
